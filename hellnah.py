c297016bbe2cc6c55a41a3eb74231dbbd66ccbb7
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-10 17:19:43 +0530

    removed testing files

--- hellnah.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-Latest_date=datetime.datetime.now().date()-datetime.timedelta(days=1)
-
-print(Latest_date)
---
+++ new.py
@@ -0,0 +1,5 @@
+import datetime
+
+Latest_date=datetime.datetime.now().date()-datetime.timedelta(days=1)
+
+print(Latest_date)
