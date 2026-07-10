007e88f9fa1a8a0f8e5135441d839bda596ec6fb
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-10 18:15:46 +0530

    risk

---
+++ Risk.py
@@ -0,0 +1,14 @@
+from values import gold_pct 
+from values import silver_pct
+from values import oil_pct
+
+
+from percentage import USD_pct
+from percentage import EUR_pct
+from percentage import JPY_pct
+from percentage import GBP_pct
+from percentage import CNY_pct
+from percentage import AUD_pct
+
+
+
