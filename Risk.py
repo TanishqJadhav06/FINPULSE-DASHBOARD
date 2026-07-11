f55cb20d140751f05cce99a25880a6b428aebde3
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-11 15:00:41 +0530

    bugs

--- Risk.py
+++ Risk.py
@@ -1,7 +1,7 @@
 from values import gold_pct 
 from values import silver_pct
 from values import oil_pct
-
+from values import naturalgas_pct
 
 from percentage import USD_pct
 from percentage import EUR_pct
@@ -9,6 +9,7 @@ from percentage import JPY_pct
 from percentage import GBP_pct
 from percentage import CNY_pct
 from percentage import AUD_pct
+
 
 
 
--- ozark.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-response2 = requests.get(
-        "https://www.alphavantage.co/query?function=NATURAL_GAS&interval=monthly&apikey=HTQRWVS672646MI7"
-    )
-print(json.dumps(response2.json(),indent=2))
