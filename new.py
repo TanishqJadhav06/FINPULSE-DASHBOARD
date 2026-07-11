eecdfe2aa30a903cec4f1dd17b1112796542ca19
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-11 12:51:33 +0530

    Commdities percantag

--- new.py
+++ new.py
@@ -1,4 +1,21 @@
 import requests
 
-response=requests.get("https://www.alphavantage.co/query?function=WHEAT&interval=monthly&apikey=HTQRWVS672646MI7")
-print(response.json())
+API_TOKEN = "6a51e7df476f21.98560786"
+
+def get_commodity(code, interval="daily"):
+    url = f"https://eodhd.com/api/commodities/historical/{code}"
+    params = {"api_token": API_TOKEN, "interval": interval}
+    response = requests.get(url, params=params)
+    return response.json()
+
+# Monthly WTI prices
+data = get_commodity("WTI")
+print(f"Commodity: {data['meta']['name']}")
+print(f"Unit: {data['meta']['unit']}")
+print(f"Total records: {data['meta']['total']}")
+
+for row1 in data["data"][:1]:
+    print(f"  {row1['date']}: ${row1['value']}")
+
+for row2 in data["data"][1:2]:
+    print(f"  {row2["date"]}: ${row2['value']}")
