1408f1462887f89733881d3b4ccea9b16ebaeb09
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-12 11:18:27 +0530

    bugs fixture

--- Risk.py
+++ Risk.py
@@ -10,6 +10,10 @@ from percentage import GBP_pct
 from percentage import CNY_pct
 from percentage import AUD_pct
 
-
-
+from markets import nifty_pct
+from markets import nasdaq_pct
+from markets import sp500_pct
+from markets import dowj_pct
+from markets import banknifty_pct
+from markets import sensex_pct
 
