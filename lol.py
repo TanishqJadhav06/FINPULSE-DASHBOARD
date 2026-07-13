e22dc8fabec7c08edf8d7173cf1e5c30e3632203
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-13 19:23:12 +0530

    markets program percantage changes is used in risk to get overall market summary

--- lol.py
+++ lol.py
@@ -1,7 +1,52 @@
-import requests
+import yfinance as yf
 
-import json
 
-score=-1.5
-confidence=(abs(score)/38)*100
-print(confidence)
+for name, symbol in {"NIFTY50": "^NSEI"}.items():
+        t = yf.Ticker(symbol)
+        niftyprice = t.fast_info["lastPrice"]
+        niftyprev = t.fast_info["previousClose"]
+        nifty_pct=((niftyprice-niftyprev)/niftyprev)*100
+        print(f"{name}    {niftyprice:.2f} {nifty_pct:+.2f}%")
+
+
+for name, symbol in {"NASDAQ": "^IXIC"}.items():
+        t = yf.Ticker(symbol)
+        nasprice = t.fast_info["lastPrice"]
+        nasprev = t.fast_info["previousClose"]
+        nasdaq_pct=((nasprice-nasprev)/nasprev)*100
+        print(f"{name}     {nasprice:.2f} {nasdaq_pct:+.2f}%")
+
+
+for name, symbol in {"S&P500": "^GSPC"}.items():
+        t = yf.Ticker(symbol)
+        sp500price = t.fast_info["lastPrice"]
+        sp500prev = t.fast_info["previousClose"]
+        sp500_pct=((sp500price-sp500prev)/sp500prev)*100
+        print(f"{name}     {sp500price:.2f}  {sp500_pct:+.2f}%")
+
+
+for name, symbol in {"DOWJONES": "^DJI"}.items():
+        t = yf.Ticker(symbol)
+        dowjprice = t.fast_info["lastPrice"]
+        dowjprev = t.fast_info["previousClose"]
+        dowj_pct=((dowjprice-dowjprev)/dowjprev)*100
+        print(f"{name}   {dowjprice:.2f}  {dowj_pct:+.2f}%")
+
+for name, symbol in {"BANKNIFTY":"^NSEBANK"}.items():
+        t = yf.Ticker(symbol)
+        bankniftyprice = t.fast_info["lastPrice"]
+        bankniftyprev = t.fast_info["previousClose"]
+        banknifty_pct=((bankniftyprice-bankniftyprev)/bankniftyprev)*100
+        print(f"{name}  {bankniftyprice:.2f}  {banknifty_pct:+.2f}%")
+
+for name, symbol in {"SENSEX":"^BSESN"}.items():
+        t = yf.Ticker(symbol)
+        sensexprice = t.fast_info["lastPrice"]
+        sensexprev = t.fast_info["previousClose"]
+        sensex_pct=((sensexprice-sensexprev)/sensexprev)*100
+        print(f"{name}     {sensexprice:.2f}  {sensex_pct:+.2f}%")
+
+
+
+
+
--- markets.py
+++ markets.py
@@ -1,21 +1,63 @@
-from config import indices
 import yfinance as yf
 
 
 def market():
+    global nifty_pct
+    global nasdaq_pct
+    global sp500_pct
+    global dowj_pct
+    global banknifty_pct
+    global sensex_pct
+    
+
     print("📊 3. Global MARKETS")
     print("--------------------------")
     print("Index\t   Value     Change")
     print("--------------------------")
-    for name, symbol in indices.items():
-        try:
-            t = yf.Ticker(symbol)
-            price = t.fast_info["lastPrice"]
-            prev = t.fast_info["previousClose"]
-            pct = (price - prev) / prev * 100
-            print(f"{name:<10} {price:.2f}  {pct:+.2f}%")
-        except Exception as e:
-            print(f"{name:<10} error: {e}")
+    for name, symbol in {"NIFTY50": "^NSEI"}.items():
+        t = yf.Ticker(symbol)
+        niftyprice = t.fast_info["lastPrice"]
+        niftyprev = t.fast_info["previousClose"]
+        nifty_pct=((niftyprice-niftyprev)/niftyprev)*100
+        print(f"{name}    {niftyprice:.2f} {nifty_pct:+.2f}%")
+
+
+    for name, symbol in {"NASDAQ": "^IXIC"}.items():
+        t = yf.Ticker(symbol)
+        nasprice = t.fast_info["lastPrice"]
+        nasprev = t.fast_info["previousClose"]
+        nasdaq_pct=((nasprice-nasprev)/nasprev)*100
+        print(f"{name}     {nasprice:.2f} {nasdaq_pct:+.2f}%")
+
+
+    for name, symbol in {"S&P500": "^GSPC"}.items():
+        t = yf.Ticker(symbol)
+        sp500price = t.fast_info["lastPrice"]
+        sp500prev = t.fast_info["previousClose"]
+        sp500_pct=((sp500price-sp500prev)/sp500prev)*100
+        print(f"{name}     {sp500price:.2f}  {sp500_pct:+.2f}%")
+
+
+    for name, symbol in {"DOWJONES": "^DJI"}.items():
+        t = yf.Ticker(symbol)
+        dowjprice = t.fast_info["lastPrice"]
+        dowjprev = t.fast_info["previousClose"]
+        dowj_pct=((dowjprice-dowjprev)/dowjprev)*100
+        print(f"{name}   {dowjprice:.2f}  {dowj_pct:+.2f}%")
+
+    for name, symbol in {"BANKNIFTY":"^NSEBANK"}.items():
+        t = yf.Ticker(symbol)
+        bankniftyprice = t.fast_info["lastPrice"]
+        bankniftyprev = t.fast_info["previousClose"]
+        banknifty_pct=((bankniftyprice-bankniftyprev)/bankniftyprev)*100
+        print(f"{name}  {bankniftyprice:.2f}  {banknifty_pct:+.2f}%")
+
+    for name, symbol in {"SENSEX":"^BSESN"}.items():
+        t = yf.Ticker(symbol)
+        sensexprice = t.fast_info["lastPrice"]
+        sensexprev = t.fast_info["previousClose"]
+        sensex_pct=((sensexprice-sensexprev)/sensexprev)*100
+        print(f"{name}     {sensexprice:.2f}  {sensex_pct:+.2f}%")
 
 
 market()
