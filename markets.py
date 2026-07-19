75c595f8692fb64dac92ce61d0e19bf95f442a07
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-19 15:01:41 +0530

    added rich interface to markets indics

--- markets.py
+++ markets.py
@@ -7,8 +7,8 @@ dowj_pct=0
 banknifty_pct=0
 sensex_pct=0
 
-from rich.console import Console
-console=Console()
+
+
 def market():
     global nifty_pct
     global nasdaq_pct
@@ -16,26 +16,33 @@ def market():
     global dowj_pct
     global banknifty_pct
     global sensex_pct
-    
 
-    console.print("📊 3. Global MARKETS",style="yellow")
+    from rich.console import Console
+    from rich.table import Table
+    console=Console()
+    table=Table(title="📊 3. Global MARKETS")
     print("--------------------------")
-    print("Index\t   Value     Change")
-    print("--------------------------")
+    table.add_column("Index",style="bold cyan")
+    table.add_column("Value",style="bold white")
+    table.add_column("Change",style="bold green")
+
     for name, symbol in {"NIFTY50": "^NSEI"}.items():
         t = yf.Ticker(symbol)
         niftyprice = t.fast_info["lastPrice"]
         niftyprev = t.fast_info["previousClose"]
         nifty_pct=((niftyprice-niftyprev)/niftyprev)*100
-        print(f"{name}    {niftyprice:.2f} {nifty_pct:+.2f}%")
-
+        nifty_pctn=(f"{nifty_pct:+.2f}%")
+        niftyrate=str(f"{niftyprice:.2f}")
+        table.add_row(name,niftyrate,nifty_pctn)
 
     for name, symbol in {"NASDAQ": "^IXIC"}.items():
         t = yf.Ticker(symbol)
         nasprice = t.fast_info["lastPrice"]
         nasprev = t.fast_info["previousClose"]
         nasdaq_pct=((nasprice-nasprev)/nasprev)*100
-        print(f"{name}     {nasprice:.2f} {nasdaq_pct:+.2f}%")
+        nasdaq_pctn=(f"{nasdaq_pct:+.2f}%")
+        nasdaqrate=str(f"{nasprice:.2f}")
+        table.add_row(name,nasdaqrate,nasdaq_pctn)
 
 
     for name, symbol in {"S&P500": "^GSPC"}.items():
@@ -43,7 +50,9 @@ def market():
         sp500price = t.fast_info["lastPrice"]
         sp500prev = t.fast_info["previousClose"]
         sp500_pct=((sp500price-sp500prev)/sp500prev)*100
-        print(f"{name}     {sp500price:.2f}  {sp500_pct:+.2f}%")
+        sp500_pctn=(f"{sp500_pct:+.2f}%")
+        sp500rate=str(f"{sp500price:.2f}")
+        table.add_row(name,sp500rate,sp500_pctn)
 
 
     for name, symbol in {"DOWJONES": "^DJI"}.items():
@@ -51,21 +60,29 @@ def market():
         dowjprice = t.fast_info["lastPrice"]
         dowjprev = t.fast_info["previousClose"]
         dowj_pct=((dowjprice-dowjprev)/dowjprev)*100
-        print(f"{name}   {dowjprice:.2f}  {dowj_pct:+.2f}%")
+        dowj_pctn=(f"{dowj_pct:+.2f}%")
+        dowjrate=str(f"{dowjprice:.2f}")
+        table.add_row(name,dowjrate,dowj_pctn)
 
     for name, symbol in {"BANKNIFTY":"^NSEBANK"}.items():
         t = yf.Ticker(symbol)
         bankniftyprice = t.fast_info["lastPrice"]
         bankniftyprev = t.fast_info["previousClose"]
         banknifty_pct=((bankniftyprice-bankniftyprev)/bankniftyprev)*100
-        print(f"{name}  {bankniftyprice:.2f}  {banknifty_pct:+.2f}%")
+        banknifty_pctn=(f"{banknifty_pct:+.2f}%")
+        bankniftyrate=str(f"{bankniftyprice:.2f}")
+        table.add_row(name,bankniftyrate,banknifty_pctn)
 
     for name, symbol in {"SENSEX":"^BSESN"}.items():
         t = yf.Ticker(symbol)
         sensexprice = t.fast_info["lastPrice"]
         sensexprev = t.fast_info["previousClose"]
         sensex_pct=((sensexprice-sensexprev)/sensexprev)*100
-        print(f"{name}     {sensexprice:.2f}  {sensex_pct:+.2f}%")
+        sensex_pctn=(f"{sensex_pct:+.2f}%")
+        sensexrate=str(f"{sensexprice:.2f}")
+        table.add_row(name,sensexrate,sensex_pctn)
 
+        
+    console.print(table)
 if __name__=="__main__":
     market()
