8b797c03c4f075d584f149fa1a5ebe723a236921
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-19 14:39:23 +0530

    Added rich interface to preety print the ouput for currency's

--- currencies.py
+++ currencies.py
@@ -7,22 +7,44 @@ from percentage import GBP_pct
 from percentage import CNY_pct
 from percentage import AUD_pct
 
-from rich.console import Console
-console=Console()
+
 def currency():
-    console.print("🌐 2. GLOBAL CURRENCIES (vs INR)",style="bold green")
-    print("---------------------------")
     currency=currency_response.json()
 
-    print("Pair \t   Price     Change")
-    print("---------------------------")
-    print(f"USD/INR    {currency["rates"]["INR"]}     {USD_pct:.2f}%")
-    print(f"EUR/INR    {currency["rates"]["INR"]/(float(currency["rates"]["EUR"])):.2f}    {EUR_pct:.2f}%")
-    print(f"JPY/INR    {currency["rates"]["INR"]/(float(currency["rates"]["JPY"])):.2f}      {JPY_pct:.2f}%")
-    print(f"GBP/INR    {currency["rates"]["INR"]/(float(currency["rates"]["GBP"])):.2f}    {GBP_pct:.2f}%")
-    print(f"CNY/INR    {currency["rates"]["INR"]/(float(currency["rates"]["CNY"])):.2f}     {CNY_pct:.2f}%")
-    print(f"AUD/INR    {currency["rates"]["INR"]/(float(currency["rates"]["AUD"])):.2f}     {AUD_pct:.2f}%\n")
+    USD_pctn=str(f"{USD_pct:.2f}%")
+    EUR_pctn=str(f"{EUR_pct:.2f}%")
+    JPY_pctn=str(f"{JPY_pct:.2f}%")
+    GBP_pctn=str(f"{GBP_pct:.2f}%")
+    CNY_pctn=str(f"{CNY_pct:.2f}%")
+    AUD_pctn=str(f"{AUD_pct:.2f}%")
+
+
+    usdrate=str(f"{(currency["rates"]["INR"]):.2f}")
+    Eurorate=str(f"{currency["rates"]["INR"]/(float(currency["rates"]["EUR"])):.2f}")
+    JPYrate=str(f"{currency["rates"]["INR"]/(float(currency["rates"]["JPY"])):.2f}")
+    GBPrate=str(f"{currency["rates"]["INR"]/(float(currency["rates"]["GBP"])):.2f}")
+    CNYrate=str(f"{currency["rates"]["INR"]/(float(currency["rates"]["AUD"])):.2f}")
+    AUDrate=str(f"{currency["rates"]["INR"]/(float(currency["rates"]["CNY"])):.2f}")
+
+
+
+    from rich.console import Console
+    from rich.table import Table
+
+    table=Table(title="🌐 2. GLOBAL CURRENCIES (vs INR)",style="bold")
+    console=Console()
+
+    table.add_column("Pair",style="bold cyan",)
+    table.add_column("Price (INR)",style=" bold white")
+    table.add_column("Change",style=" bold green")
+    table.add_row("$💵  USD/INR",usdrate,USD_pctn)
+    table.add_row("€💶  EUR/INR",Eurorate,EUR_pctn)
+    table.add_row("Ұ💴  JPY/INR",JPYrate,JPY_pctn)
+    table.add_row("£💷  GBP/INR",GBPrate,GBP_pctn)
+    table.add_row("¥💴  CNY/INR",CNYrate,CNY_pctn)
+    table.add_row("$💵  AUD/INR",AUDrate,AUD_pctn)
     print(f"Last Updated🕒: {currency["date"]}\n")
-    
+
+    console.print(table)
 if __name__=="__main__":
     currency()
