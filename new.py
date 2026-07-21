998c7bfad1ca81c4509924d117c3e5d13272dba0
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-21 23:24:26 +0530

    testing

--- new.py
+++ new.py
@@ -1,13 +1,10 @@
 from rich.table import Table
 from rich.console import Console
 
-table=Table()
+table=Table(title="Today's market signal",title_justify="center",title_style="bold light_green")
 console=Console()
 
-console.print("Indian markets",justify="left")
-table.add_column("Index")
-table.add_column("Value")
-table.add_column("Change")
-table.add_row("maximus","19836","1.9%")
+console.print("Risk On",style="bold green")
+table.add_row("confidence")
 
 console.print(table)
