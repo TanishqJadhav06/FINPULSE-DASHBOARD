49c04e54244893761678c08d0b3db208637c1f87
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-26 21:22:18 +0530

    alignment and colour change done in 'header.py'

--- header.py
+++ header.py
@@ -54,9 +54,9 @@ def last_updated_panel():
 
 def title_block():
     content = Text(justify="center")
-    content.append("📈 FINPULSE V1.0\n", style="bold bright_green")
+    content.append("📈  FINPULSE v1.0\n", style="bold bright_green")
     content.append("Personal Macro Intelligence Terminal\n", style="bold white")
-    content.append("\nKnow the markets. Understand the big picture", style="bold dodger_blue1",)
+    content.append("\nKnow the markets. Understand the big picture", style="bold deep_sky_blue1")
     return content
 def header():
     grid = Table.grid(expand=True)
