bb5410cd5c7a3a0534658e6d99efafd30a4205e8
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-29 15:25:30 +0530

    bug:fixed the output bug in title

--- news.py
+++ news.py
@@ -7,7 +7,7 @@ def news():
     from rich.table import Table
     from rich import box
     console=Console()
-    table=Table(title="🗞️ 5. News Headlines",box=box.DOUBLE_EDGE,border_style="white")
+    table=Table(title="🗞️  5. News Headlines",box=box.DOUBLE_EDGE,border_style="white")
     table.add_column("📰  TOP MARKET HEADLINES",justify="left")
     for article in data["articles"][1:5]:
         table.add_row(f"● {str(article["title"])}\n")
