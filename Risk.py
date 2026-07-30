76d233718e4db0d74ce80754ad53272f50cba81e
TanishqJadhav06 <tanishqjadhav06@gmail.com>
2026-07-30 23:10:14 +0530

    fix: fixed small formatting for risk

--- Risk.py
+++ Risk.py
@@ -259,8 +259,8 @@ def risk():
     finalscore=str(f"{score}")
     for market in MarketMood:
         table.add_row("","Market Mood :"+market)
-    table.add_row("","Raw Score: "+finalscore)
-    table.add_row("","Confidence :" +finaconfidence+"\n")
+    table.add_row("","Raw Score :"+ finalscore)
+    table.add_row("","Confidence :" + finaconfidence+"\n")
 
     for reason in reasons:
         table.add_row(reason+"\n")
